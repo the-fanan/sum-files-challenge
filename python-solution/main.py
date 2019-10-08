105d3284a5967953235f31d380eb1dd8be14e003
Fanan Dala <fanan.dala@yahoo.com>
2019-10-08 20:09:04 +0100

    added time counter

--- python-solution/main.py
+++ python-solution/main.py
@@ -1,7 +1,10 @@
 import os
+import time
 from FileNumberSummer import FileNumberSummer
 
 # 1, 1000 because the files are named from 000001.csv to 001000.csv
+start_time = time.time()
 FNS = FileNumberSummer(os.getcwd() + "/files", 1, 1000)
 print(FNS.getSum())
-
+elapsed_time = time.time() - start_time
+print("time: ", elapsed_time)
