3fe3e439fbffb406fcfa25b3ffd7489e68c9fe3b
Fanan Dala <fanan.dala@yahoo.com>
2019-10-08 15:42:11 +0100

    second solution: optimized folder traversing

--- python-solution/FileNumberSummer.py
+++ python-solution/FileNumberSummer.py
@@ -1,8 +1,18 @@
-import os
-
 class FileNumberSummer:
-	def __init__(self, absFilesPath):
-		self.rootDir = absFilesPath
+	def __init__(self, rootDir, start, end):
+		self.rootDir = rootDir
+		self.start = start
+		self.end = end
+
+	def generateFolderName(self,value):
+		if value % 10 == 0:
+			value = value - 1
+		highest = value + (10 - (value % 10))
+		lowest = highest - 9
+		return "{0:0=6}".format(lowest) + "-" + "{0:0=6}".format(highest)
+
+	def generateFileName(self,value):
+		return "{0:0=6}".format(value) + ".csv"
 
 	def _recursivelySumNumbers(self,listOfNumbers, first, last):
 		if first == last:
@@ -26,28 +36,14 @@ class FileNumberSummer:
 		n = len(listOfNumbers)
 		return self._recursivelyHandleNewLineCaseSummer(listOfNumbers, 0, n - 1)
 	
-	def _recSumFiles(self,listOfFileNames, folderDir, first, last):
+	def sumFiles(self,first, last):
 		if first == last:
-			fileContent = open(folderDir + "/" + listOfFileNames[first], "r").read().split("\n")
+			fileContent = open(self.rootDir + "/" + self.generateFolderName(first) + "/" + self.generateFileName(first), "r").read().split("\n")
+			#return self._handleNewLineCaseSummer(fileContent)
 			return self._handleNewLineCaseSummer(fileContent)
 		else:
 			mid = (first + last) // 2
-			return (self._recSumFiles(listOfFileNames, folderDir, first, mid) + self._recSumFiles(listOfFileNames, folderDir, mid + 1, last))
-
-	def _sumFiles(self,folderDir):
-		files = os.listdir(folderDir)
-		return self._recSumFiles(files, folderDir, 0, len(files) - 1)
-
-	def _recSumFolders(self,listOfFolderNames, first, last):
-		if first == last:
-			return self._sumFiles(self.rootDir + "/" + listOfFolderNames[first])
-		else:
-			mid = (first + last) // 2
-			return (self._recSumFolders(listOfFolderNames, first, mid) + self._recSumFolders(listOfFolderNames, mid + 1, last))
-
-	def _sumFolders(self):
-		folders = os.listdir(self.rootDir)
-		return self._recSumFolders(folders, 0, len(folders) - 1)
+			return (self.sumFiles(first, mid) + self.sumFiles(mid + 1, last))
 
 	def getSum(self):
-		return self._sumFolders()
+		return self.sumFiles(self.start, self.end)
--- python-solution/main.py
+++ python-solution/main.py
@@ -1,8 +1,7 @@
 import os
 from FileNumberSummer import FileNumberSummer
-#os.chdir("../")
-rootDir = os.getcwd()
-filesDir = rootDir + "/files"
-
-FNS = FileNumberSummer(filesDir)
+os.chdir("../")
+rootDir = os.getcwd() + "/files"
+FNS = FileNumberSummer(rootDir, 1, 1000)
 print(FNS.getSum())
+
